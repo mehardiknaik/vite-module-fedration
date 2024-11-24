66143c0ac954a473453a8f9d29ceb034ccaebeb9
mehardiknaik <hardiknaik7444@gmail.com>
2024-11-24 16:32:42 +0530

    added env varibale

--- vite-host/src/components/Remote.tsx
+++ vite-host/src/components/Remote.tsx
@@ -1,16 +1,12 @@
-import  { lazy, Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-const RemoteComp = lazy(
-  async () =>
-    // @ts-ignore
-    import("remote/remote-button")
-);
+const RemoteComp = lazy(async () => import("remote/remote-button"));
 
 const Remote = () => {
   return (
-    <ErrorBoundary fallback={''}>
-      <Suspense fallback="loading...">
+    <ErrorBoundary fallback={""}>
+      <Suspense fallback={""}>
         <RemoteComp />
       </Suspense>
     </ErrorBoundary>
---
+++ vite-host/src/remote-modules.d.ts
@@ -0,0 +1,6 @@
+// remote-modules.d.ts
+declare module "remote/remote-button" {
+  import { FC } from "react";
+  const RemoteButton: FC;
+  export default RemoteButton;
+}
